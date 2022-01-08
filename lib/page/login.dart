597a8fe74801de2eb79b7dcf2bdd683cd589fd22
Ridwan Nurjaman <59529442+ridwannurjaman@users.noreply.github.com>
2022-01-08 21:37:25 +0700

    update

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -8,8 +8,129 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emails = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+  bool isLoading = false;
+  bool isChecked = false;
+  bool passwordVisible = true;
+
+  Color getColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+      MaterialState.hovered,
+      MaterialState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Colors.blue;
+    }
+    return Colors.blue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SingleChildScrollView(child: Container()));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          // margin: EdgeInsets.fromLTRB(10, 59, 10, 10),
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 200,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/cloudbig.png'),
+                        fit: BoxFit.fill)),
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                child: Form(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: emails,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            hintText: "Enter your email",
+                            labelText: "Email / Username",
+                            prefixIcon: Icon(Icons.person),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: password,
+                          obscureText: passwordVisible ? true : false,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              hintText: "Enter your password",
+                              labelText: "Password",
+                              prefixIcon: Icon(Icons.lock),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  passwordVisible
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                  color: Theme.of(context).primaryColorDark,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    passwordVisible = !passwordVisible;
+                                  });
+                                },
+                              )),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 45,
+                        child: ElevatedButton.icon(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Color(0xff036bca)),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                side: BorderSide(
+                                    color: Color(0xff036bca), width: 2.0),
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          label: Text('LOGIN'),
+                          icon: isLoading
+                              ? SizedBox(
+                                  child: CircularProgressIndicator(
+                                      color: Colors.white),
+                                  height: 20.0,
+                                  width: 20.0,
+                                )
+                              : Container(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Spacer(),
+              Image.asset(
+                'assets/logo.png',
+                height: 150,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
